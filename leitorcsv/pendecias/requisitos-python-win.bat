b1f911210a40ad35e857b08961fd1a5d7b2deb70
Lucas Perdigão De Oliveira <116082415+LucasPerolive@users.noreply.github.com>
2023-11-02 21:42:46 -0300

    Update requisitos-python-win.bat

--- leitorcsv/pendecias/requisitos-python-win.bat
+++ leitorcsv/pendecias/requisitos-python-win.bat
@@ -1,3 +1,8 @@
 pip install pymysql
+python.exe -m pip install --upgrade pip
+
 pip install sqlalchemy
+python.exe -m pip install --upgrade pip
+
 pip install pandas
+python.exe -m pip install --upgrade pip
